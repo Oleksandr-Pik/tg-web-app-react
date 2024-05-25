44fd7f57c1aab579abb4a8787f1f7760005555f8
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-05-25 15:37:01 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 import ProductList from "./components/ProductList/ProductList";
